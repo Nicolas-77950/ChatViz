4e3ecdde092e6d0754999a8cab3c17bae8007280
heidi_irdrj <heidi.iroudayaradjou@gmail.com>
2026-03-24 14:11:24 +0100

    contact

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,24 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ContactController; 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactController;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
 Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
-// -----------------------------------
 
-// Inclusion des routes organisées par fonctionnalités
+
 require __DIR__ . '/web/dashboard.php';
 
-// Routes publiques
 Route::get('/menu', function () { return view('welcome'); })->name('menu');
 Route::get('/a-propos', function () { return view('welcome'); })->name('about');
-Route::get('/contact', function () { return view('welcome'); })->name('contact');
 Route::get('/safe-zone', function () { return view('welcome'); })->name('safe-zone');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
