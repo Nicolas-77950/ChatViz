93f3f9ed42fc76c201ff71b5c55fb78cc161da48
heidi_irdrj <heidi.iroudayaradjou@gmail.com>
2026-03-24 15:10:25 +0100

    contact

--- routes/web.php
+++ routes/web.php
@@ -9,21 +9,24 @@ Route::get('/', function () {
 });
 
 
-Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 
 
 require __DIR__ . '/web/dashboard.php';
 
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 Route::get('/menu', function () { return view('welcome'); })->name('menu');
 Route::get('/a-propos', function () { return view('welcome'); })->name('about');
 Route::get('/safe-zone', function () { return view('welcome'); })->name('safe-zone');
 
 
 Route::middleware('auth')->group(function () {
+    Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
+
+
